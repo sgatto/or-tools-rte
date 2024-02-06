e750c556ccd8efd9c02558de4b50df6f16c3838d
Andrea Sgattoni <andrea.sgattoni@rte-france.com>
2024-02-06 11:57:43 +0100

    update-5 from google main: cmake

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,4 +1,4 @@
-# Copyright 2010-2022 Google LLC
+# Copyright 2010-2024 Google LLC
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -143,6 +143,16 @@ endif()
 # Check optional Dependencies
 if(USE_CPLEX)
   find_package(CPLEX REQUIRED)
+endif()
+
+# CXX Test
+if(BUILD_TESTING)
+  if(NOT BUILD_googletest)
+    find_package(GTest REQUIRED)
+  endif()
+  if(NOT TARGET GTest::gtest_main)
+    message(FATAL_ERROR "Target GTest::gtest_main not available.")
+  endif()
 endif()
 
 if (USE_SIRIUS)

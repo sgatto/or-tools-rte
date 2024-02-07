de10e2f33d350d2666896f7a6c375dfe8a0409f1
Andrea Sgattoni <andrea.sgattoni@rte-france.com>
2024-02-07 14:44:56 +0100

    try fix test

--- ortools/linear_solver/xpress_interface_test.cc
+++ ortools/linear_solver/xpress_interface_test.cc
@@ -728,14 +728,13 @@ TEST_F(XpressFixtureMIP, Write) {
   tmpBuffer << tmpFile.rdbuf();
   tmpFile.close();
   std::filesystem::remove_all(temporary_working_dir);
-  // clang-format off
-  EXPECT_EQ(tmpBuffer.str(), R"(NAME          newProb
+  EXPECT_EQ(tmpBuffer.str(), std::string(R"(NAME          newProb
 OBJSENSE  MAXIMIZE
 ROWS
  N  __OBJ___
- L  R1
- L  R2
-COLUMNS
+ L  R1)")+"      \n"+
+" L  R2"+"      \n"+
+std::string(R"(COLUMNS
     C1        __OBJ___  1
     C1        R1        3
     C2        __OBJ___  2
@@ -752,8 +751,7 @@ BOUNDS
  UP BND00001  C2        5.147593849384714
  LO BND00001  C2        -1
 ENDATA
-)");
-// clang-format on
+)"));
 }
 
 TEST_F(XpressFixtureLP, SetPrimalTolerance) {

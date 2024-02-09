27580520adc9c2080798d7b9dd7edc33e7952ed8
Andrea Sgattoni <andrea.sgattoni@rte-france.com>
2024-02-09 12:10:44 +0100

    try fix encoding on windows

--- patch.py
+++ patch.py
@@ -3,11 +3,11 @@ from typing import List
 from dataclasses import dataclass
 
 def replace_in_file(filepath, search, replace):
-    with open(filepath, 'r') as file:
+    with open(filepath, 'r', encoding="utf8") as file:
         data = file.read() 
         data = data.replace(search, replace) 
 
-    with open(filepath, 'w') as file:
+    with open(filepath, 'w', encoding="utf8") as file:
         file.write(data) 
 
 
